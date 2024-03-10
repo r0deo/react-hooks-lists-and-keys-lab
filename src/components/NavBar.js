4eca971cf8152d30d7318cb0bad302ad3f034d00
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-03-10 18:01:13 +0300

    assignment completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,20 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return(
+    <div>
+         <nav>
+         {links.map((link, index) => (
+          <a key={index} href={`#${link}`}>
+            {link}
+          </a>
+        ))}
+
+         </nav>
+
+
+    </div>
+  );
 }
 
 export default NavBar;
